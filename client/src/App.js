ab53baf54d416707e3a6cef53790d0292d3e65d3
Jim Doan <jimdoan4@gmail.com>
2019-10-13 18:09:01 -0400

    added styling for main home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,321 @@
-import React from 'react';
+import React, { Component } from "react";
 import Nav from "./components/Nav";
 
-function App() {
-  return (
-    <div>
-   <Nav />
-   <header className="main-header">
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div>
+          <header className="main-header">
           </header>
-    </div>
-  );
+
+          <section id="order-section" style={{ marginTop: "280px;" }}>
+            <div className="container text-center">
+              <h4 className="order-title">New & Trending Videos</h4>
+
+              <div className="row">
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="http://www.thebrokaw.com/uploads/8/6/7/3/86738218/published/dora-version-3_1.jpg?1566406566"
+                        alt="Idea"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Dora The Explorer</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://cectheatres.com/omniweb/poster_715354482.jpg"
+                        alt="Support"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Abominable Man</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://media.cinemark.com/media/75959311/md_spider_man_far_from_home_extended_cut.jpg"
+                        alt="Encourage"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Spider Man</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://m.media-amazon.com/images/M/MV5BMjI1NDYzNzY2Ml5BMl5BanBnXkFtZTgwODQwODczNTM@._V1_.jpg"
+                        alt="Idea"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">The Greatest Showman</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="order-section" style={{ marginTop: "270px;" }}>
+            <div className="container text-center">
+              <h4 className="order-title">Kids Movies</h4>
+              <div className="row">
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://www.todaysparent.com/wp-content/uploads/2018/04/best-kids-movies-us-netflix-charlie-and-the-chocolate-factory.jpg"
+                        alt="Idea"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Charlie and the Chocolate Factory</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://redtricom.files.wordpress.com/2017/05/ugly-dolls.jpg?w=605"
+                        alt="Support"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Ugly Dolls</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-kids-movies-2019-toy-story-4-1555083312.jpg"
+                        alt="Encourage"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Toy Story 4</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kids-movies-2019-how-to-train-your-dragon-1543261100.jpg?crop=1.00xw:0.949xh;0,0.0505xh&resize=480:*"
+                        alt="Idea"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">How to Train Your Dragon</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>{" "}
+            </div>
+          </section>
+
+          <section id="order-section" style={{ marginTop: "270px;" }}>
+            <div className="container text-center">
+              <h4 className="order-title">New & Trending Videos</h4>
+
+              <div className="row">
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kids-movies-2019-the-kid-who-would-be-king-1543259868.jpg?crop=0.9876543209876543xw:1xh;center,top&resize=480:*"
+                        alt="Idea"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">The Kid Who Would Be King</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://pbs.twimg.com/media/CGXtzRrWoAA9mED.jpg"
+                        alt="Support"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">The Avengers</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://s31395.pcdn.co/wp-content/uploads/2016/02/travel_movies_for_kids_finding_nemo.png"
+                        alt="Encourage"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Finding Nemo</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://thenewscrunch.com/wp-content/uploads/2019/09/Screenshot-2019-09-13-at-10.42.31-AM.png"
+                        alt="Idea"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Paramaguru</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="order-section" style={{ marginTop: "270px;" }}>
+            <div className="container text-center">
+              <h4 className="order-title">Scary & Suspense Videos</h4>
+
+              <div className="row">
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://upload.wikimedia.org/wikipedia/en/thumb/2/29/Movie_poster_for_%22Scary_Movie%22.jpg/220px-Movie_poster_for_%22Scary_Movie%22.jpg"
+                        alt="Idea"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Scary Movie</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://www.indiewire.com/wp-content/uploads/2017/05/the-descent-2005.jpg?w=674"
+                        alt="Support"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">The Descent</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div className="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://resizing.flixster.com/xzo0e_KgG8zZ2WUEwEcLgrub8qc=/180x267/v1.bTsxMjg4NDYxMDtqOzE4MjY5OzIwNDg7MjAyNTszMDAw"
+                        alt="Encourage"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Escape Room</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <div class="card ordering">
+                    <div className="icon-container">
+                      <img
+                        src="https://cdn3.movieweb.com/i/movie/qQD9MBpolzQqa0XhNixkXHegdnXp3A/384:50/Annabelle-Comes-Home.jpg"
+                        alt="Idea"
+                        width="255px"
+                        height="280px"
+                        className="order-picture"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h4 className="card-title">Annabelle</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
